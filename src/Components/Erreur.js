8563baa52a815ee5d6768b585c21f9f1723f3401
2kne <50320405+2kne@users.noreply.github.com>
2019-05-15 12:32:05 +0200

    First commit

---
+++ src/Components/Erreur.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class Erreur extends Component {
+
+    render() {
+        return (
+           <div>Erreurrrrrr</div>
+        )
+    }
+}
+
+export default Erreur;
